a484503df51abd29a76f6ec73936eaa61db821a1
Pasindu Vihangana <pasi1028@gmail.com>
2023-06-29 18:19:46 +0530

    contact page configured
    contact page configured to send emails

--- assets/js/contact-form-script.js
+++ assets/js/contact-form-script.js
@@ -25,19 +25,21 @@
         var message = $("#message").val();
 
 
-        $.ajax({
-            type: "POST",
-            url: "assets/php/form-process.php",
-            data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&phone_number=" + phone_number + "&message=" + message,
-            success: function (text) {
-                if (text == "success") {
-                    formSuccess();
-                } else {
-                    formError();
-                    submitMSG(false, text);
-                }
-            }
-        });
+        sendEmail(name, email, phone_number, msg_subject, message);
+
+        // $.ajax({
+        //     type: "POST",
+        //     url: "assets/php/form-process.php",
+        //     data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&phone_number=" + phone_number + "&message=" + message,
+        //     success: function (text) {
+        //         if (text == "success") {
+        //             formSuccess();
+        //         } else {
+        //             formError();
+        //             submitMSG(false, text);
+        //         }
+        //     }
+        // });
     }
 
     function formSuccess() {
@@ -58,5 +60,23 @@
             var msgClasses = "h4 text-danger";
         }
         $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+
+    // ref: https://www.youtube.com/watch?v=sGQSz22U8VM
+    function sendEmail(_name, _email, _phone, _msg_subject, _message) {
+        Email.send({
+            SecureToken: "2009fb93-ccd5-4e36-85ec-7f41ca0abce9",
+            To: 'pasi1028@gmail.com',
+            From: "pasindu@srqrobotics.com",
+            Subject: "Get In Touch",
+            Body: "        <b>Name:</b> " + _name
+                + "<br>   <b>Email:</b> " + _email
+                + "<br>   <b>Phone:</b> " + _phone
+                + "<br> <b>Subject:</b> " + _msg_subject
+                + "<br> <b>Message:</b> <br>" + _message
+
+        }).then(
+            message => alert(message)
+        );
     }
 }(jQuery)); // End of use strict
--- assets/php/form-process.php
+++ assets/php/form-process.php
@@ -65,14 +65,13 @@ $Body .= "\n";
 $success = mail($EmailTo, $Subject, $Body);
 
 // redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
+if ($success && $errorMSG == "") {
+    echo "success";
+} else {
+    if ($errorMSG == "") {
         echo "Something went wrong :(";
     } else {
         echo $errorMSG;
     }
 }
-
 ?>
