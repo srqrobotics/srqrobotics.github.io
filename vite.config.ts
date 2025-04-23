d916014fd3bc002e854a0665a84283b56955ba5d
Pasindu Vihangana <pasi1028@gmail.com>
2025-04-23 18:42:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/srqrobotics.github.io/", // ← Set this to your GitHub repo name
+  base: "/", // ← Set this to your GitHub repo name
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
